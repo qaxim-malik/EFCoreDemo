8c32e2fcac857a19d7c5b7d3ac3068f4b21ae91a
Qasim Malik <qaximmalik63@gmail.com>
2024-08-28 12:25:13 +0500

    Changed response message

--- Controllers/EFCoreDemoController.cs
+++ Controllers/EFCoreDemoController.cs
@@ -43,7 +43,7 @@ public class EFCoreDemoController : ControllerBase
         }
         else
         {
-            return BadRequest();
+            return BadRequest("There was an error");
         }
     }
 
@@ -66,7 +66,7 @@ public class EFCoreDemoController : ControllerBase
         }
         else
         {
-            return BadRequest();
+            return BadRequest("There was an error");
         }
     }
 
@@ -242,7 +242,7 @@ public class EFCoreDemoController : ControllerBase
         }
         else
         {
-            return BadRequest();
+            return BadRequest("There was an error");
         }
     }
 
@@ -263,7 +263,7 @@ public class EFCoreDemoController : ControllerBase
         }
         else
         {
-            return BadRequest();
+            return BadRequest("There was an error");
         }
     }
 }
